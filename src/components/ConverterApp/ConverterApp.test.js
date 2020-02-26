2ca7c408e6cfacb61959e2c0004b997efcf26e15
karplei <karolis.pleiris@gmail.com>
2020-02-26 10:51:51 +0200

    Fixed default test

--- src/components/ConverterApp/ConverterApp.test.js
+++ src/components/ConverterApp/ConverterApp.test.js
@@ -1,6 +1,9 @@
-/* import React from 'react';
+import React from 'react';
 import { render } from '@testing-library/react';
 import ConverterApp from './ConverterApp';
 
-test('renders ConverterApp', () => {
-}); */
+test('renders ConverterApp with BTC field', () => {
+  const { getByText } = render(<ConverterApp />);
+  const btcLabel = getByText('BTC');
+  expect(btcLabel).toBeInTheDocument();
+});
