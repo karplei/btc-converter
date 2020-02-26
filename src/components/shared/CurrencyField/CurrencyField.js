93fbf16f929097ee6512c228cfe3752298c3508f
karplei <karolis.pleiris@gmail.com>
2020-02-26 10:24:24 +0200

    Made form to scale according to screen size

--- src/components/shared/CurrencyField/CurrencyField.js
+++ src/components/shared/CurrencyField/CurrencyField.js
@@ -5,9 +5,9 @@ import './CurrencyField.css';
 const CurrencyField = ({ onCurrencyRemove, currencyName, value }) => {
   const formattedValue = new Intl.NumberFormat(navigator.language, { style: 'currency', currency: currencyName }).format(value);
   return (
-    <div className="currency-field">
+    <div className="currency-row">
       <span>{currencyName}</span>
-      <span>{formattedValue}</span>
+      <span className="currency-value">{formattedValue}</span>
       <button
         type="button"
         className="remove-button"
